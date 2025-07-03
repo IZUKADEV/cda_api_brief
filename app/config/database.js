ed35716809bc95ec1407bbc9f75757f7ddf75c1b
dimitri couvreur <pandaaxytb@gmail.com>
2025-07-04 00:57:08 +0200

    refresh

--- app/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize')
-
-const sequelize = new Sequelize( {
-    dialect: 'sqlite',
-    storage: 'carambar.sqlite',
-    logging: false
-})
-
-module.exports = sequelize;
--- app/server.js
+++ app/server.js
@@ -6,13 +6,12 @@
 // On appelle la lib express
 const express = require("express")
 const app = express()
-
-
 // app = express et app on va l'utiliser ultérieurement
 
 
-// on récupère la route Post
+// on récupère la route blagues
 app.use("/blagues", require("./routes/blagues.routes"))
+// on va pouvoir récupèrer la route post pour ajouter des blagues
 
 // On va lancer le serveur en local avec la méthode listen 
 // D'abord on définit le port d'utilisation ->
