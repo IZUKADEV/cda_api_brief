e72ffb51c379997ad58fb4a306202c2871f922e7
PandaaxY <pandaaxytb@gmail.com>
2025-07-07 13:25:07 +0200

    test bdd

--- app/bdd/database.js
+++ app/bdd/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize') // On init l'ORM
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './carambar.sqlite',
+  storage: 'app/bdd/carambar.sqlite',
   logging: false
 });
 
