e45a37e7090de95ec40f581dd59346cbd7242f5e
PandaaxY <pandaaxytb@gmail.com>
2025-07-08 10:01:24 +0200

    test passed

--- app/tests/blagues.test.js
+++ app/tests/blagues.test.js
@@ -3,12 +3,16 @@ import express from 'express'
 import { GetAll } from '../controllers/blaguesControl.js'
 import { vi } from 'vitest'
 
-// Mock du modèle Blagues
 vi.mock('../models', () => ({
   Blagues: {
     findAll: vi.fn(() =>
       Promise.resolve([
-        { id: 1, question: 'Pourquoi ?', reponse: 'Parce que.' },
+        {
+          id: 1,
+          content: 'Quel est le comble pour un marin ? Avoir le nez qui coule',
+          createdAt: '2025-07-07T11:24:25.044Z',
+          updatedAt: '2025-07-07T11:24:25.044Z'
+        },
       ]),
     ),
   },
@@ -22,7 +26,12 @@ describe('GET /blagues', () => {
     const res = await request(app).get('/blagues')
     expect(res.status).toBe(200)
     expect(res.body).toEqual([
-      { id: 1, question: 'Pourquoi ?', reponse: 'Parce que.' },
+      {
+        id: 1,
+        content: 'Quel est le comble pour un marin ? Avoir le nez qui coule',
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+      },
     ])
   })
 })
